68563c4ac04962e0b93525f259952a58b9023b64
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-22 15:27:06 +0300

    Added 9-insert.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module is used to insert a document in a collection
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert a document in a collection
+    """
+    return mongo_collection.insert_one(kwargs)
+
+
+if __name__ == "__main__":
+    insert_school(mongo_collection, kwargs)
