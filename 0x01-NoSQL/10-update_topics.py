cfdad6d13a7fa8126a73768cf3671c24b333ddd2
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-22 15:34:28 +0300

    Added 10-update.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+""" this module is used to update a document in a collection
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """update a document in a collection
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
